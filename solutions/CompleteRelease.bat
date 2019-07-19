424e0a5bbe19f85e684953c573aaba2a757adf36
JavidPack <javidpack@gmail.com>
2019-07-18 18:23:54 -0600

    v0.6 release, updated to tModLoader 0.11.2.2

--- solutions/CompleteRelease.bat
+++ solutions/CompleteRelease.bat
@@ -1,7 +1,7 @@
 :: After Pulling, Patching, and making sure the version number is changed in src, this bat will compile and create zips for all release.
 :: It will also create a zip for ExampleMod
 
-set version=v0.5.4
+set version=v0.6
 set exeFile=TerraCustom %version%.exe
 set destinationFolder=.\TerraCustom %version% Release
 
@@ -21,27 +21,19 @@ mkdir "%destinationFolder%\TerraCustom Mac %version%"
 mkdir "%destinationFolder%\TerraCustom Linux %version%"
 
 :: Windows release
-copy ..\src\TerraCustom\bin\x86\WindowsRelease\Terraria.exe "%destinationFolder%\TerraCustom Windows %version%\%exeFile%" /y
+copy ..\src\TerraCustom\bin\WindowsRelease\net45\Terraria.exe "%destinationFolder%\TerraCustom Windows %version%\%exeFile%" /y
 copy ReleaseExtras\README_Windows.txt "%destinationFolder%\TerraCustom Windows %version%\README.txt" /y
 
 call zipjs.bat zipDirItems -source "%destinationFolder%\TerraCustom Windows %version%" -destination "%destinationFolder%\TerraCustom Windows %version%.zip" -keep yes -force yes
 
 :: Mac release
-copy ..\src\TerraCustom\bin\x86\MacRelease\Terraria.exe "%destinationFolder%\TerraCustom Mac %version%\%exeFile%" /y
-:: References
-copy ..\references\MP3Sharp.dll "%destinationFolder%\TerraCustom Mac %version%\MP3Sharp.dll" /y
-copy ..\references\Ionic.Zip.Reduced.dll "%destinationFolder%\TerraCustom Mac %version%\Ionic.Zip.Reduced.dll" /y
-copy ..\references\Mono.Cecil.dll "%destinationFolder%\TerraCustom Mac %version%\Mono.Cecil.dll" /y
+copy ..\src\TerraCustom\bin\MacRelease\net45\Terraria.exe "%destinationFolder%\TerraCustom Mac %version%\%exeFile%" /y
 copy ReleaseExtras\README_Mac.txt "%destinationFolder%\TerraCustom Mac %version%\README.txt" /y
 
 call zipjs.bat zipDirItems -source "%destinationFolder%\TerraCustom Mac %version%" -destination "%destinationFolder%\TerraCustom Mac %version%.zip" -keep yes -force yes
 
 :: Linux release
-copy ..\src\TerraCustom\bin\x86\LinuxRelease\Terraria.exe "%destinationFolder%\TerraCustom Linux %version%\%exeFile%" /y
-:: References
-copy ..\references\MP3Sharp.dll "%destinationFolder%\TerraCustom Linux %version%\MP3Sharp.dll" /y
-copy ..\references\Ionic.Zip.Reduced.dll "%destinationFolder%\TerraCustom Linux %version%\Ionic.Zip.Reduced.dll" /y
-copy ..\references\Mono.Cecil.dll "%destinationFolder%\TerraCustom Linux %version%\Mono.Cecil.dll" /y
+copy ..\src\TerraCustom\bin\LinuxRelease\net45\Terraria.exe "%destinationFolder%\TerraCustom Linux %version%\%exeFile%" /y
 copy ReleaseExtras\README_Linux.txt "%destinationFolder%\TerraCustom Linux %version%\README.txt" /y
 
 call zipjs.bat zipDirItems -source "%destinationFolder%\TerraCustom Linux %version%" -destination "%destinationFolder%\TerraCustom Linux %version%.zip" -keep yes -force yes
--- solutions/buildRelease.bat
+++ solutions/buildRelease.bat
@@ -1,6 +1,9 @@
-msbuild TerraCustom.sln /p:Configuration=WindowsRelease /p:Platform="x86"
+msbuild TerraCustom.sln /restore /t:clean /p:Configuration=WindowsRelease
+msbuild tModLoader.sln /t:clean /p:Configuration=MacRelease
+msbuild tModLoader.sln /t:clean /p:Configuration=LinuxRelease
+msbuild TerraCustom.sln /p:Configuration=WindowsRelease
 @IF %ERRORLEVEL% NEQ 0 (EXIT /B %ERRORLEVEL%)
-msbuild TerraCustom.sln /p:Configuration=MacRelease /p:Platform="x86"
+msbuild TerraCustom.sln /p:Configuration=MacRelease
 @IF %ERRORLEVEL% NEQ 0 (EXIT /B %ERRORLEVEL%)
-msbuild TerraCustom.sln /p:Configuration=LinuxRelease /p:Platform="x86"
+msbuild TerraCustom.sln /p:Configuration=LinuxRelease
 @IF %ERRORLEVEL% NEQ 0 (EXIT /B %ERRORLEVEL%)
